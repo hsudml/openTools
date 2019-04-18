1ca40cb46dee88bcd57923e23ef8cd1f0b7269fe
Tim Miller <twm2@humboldt.edu>
2019-04-17 18:27:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ for (var i = 0, len = anchors.length; i < len; i++) {
 	var anchorText = anchors[i].innerText;
 	var anchorName = anchor.getAttribute('id');
 	var menu = document.getElementsByTagName('header')[0];
-	var menuList = menu.getElementsByTagName('h1')[0];
+	var menuList = menu.getElementsByTagName('h2')[0];
 	menuList.insertAdjacentHTML('afterend', '<p><a href="#'+anchorName+'" class="menuLink">'+anchorText+'</a></p>');
 }
